93fcafd965409bde19493411b4b1157ff28fef64
Luca Lee <30341309+lucalee-1@users.noreply.github.com>
2022-06-12 11:03:05 +0900

    Add tests for HTTP GET requests to '/api/blogs' -exercise 4.8

---
+++ tests/blogApi.test.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const helper = require('./testHelper');
+const app = require('../app');
+
+const api = supertest(app);
+const Blog = require('../models/blog');
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs);
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+test('blogs are returned as json', async () => {
+  const res = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+});
+
+test('all blogs are returned', async () => {
+  const res = await api.get('/api/blogs');  
+  expect(res.body).toHaveLength(helper.initialBlogs.length);
+});
