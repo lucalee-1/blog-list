843f35a3a1638670a0744c9d97858dca7ad63d3c
Luca Lee <30341309+lucalee-1@users.noreply.github.com>
2022-06-13 11:26:58 +0900

    New blog entries are associated with an user, blog creator's user info is displayed and user's created blog entries are displayed -exercise 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,9 +1,10 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (req, res, next) => {
   try {
-    const blogs = await Blog.find({});
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 });
     res.json(blogs);
   } catch (error) {
     next(error);
@@ -11,15 +12,22 @@ blogsRouter.get('/', async (req, res, next) => {
 });
 
 blogsRouter.post('/', async (req, res, next) => {
-  const blog = new Blog({
-    title: req.body.title,
-    author: req.body.author,
-    url: req.body.url,
-    likes: req.body.likes || '0',
-  });
   try {
-    const result = await blog.save();
-    res.status(201).json(result);
+    const user = await User.findById(req.body.userId);
+
+    const blog = new Blog({
+      title: req.body.title,
+      author: req.body.author,
+      url: req.body.url,
+      likes: req.body.likes || '0',
+      user: user._id,
+    });
+
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
+    res.status(201).json(savedBlog);
   } catch (error) {
     next(error);
   }
