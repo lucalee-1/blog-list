9cf765a918c806220134062a50b384d7c2a3c196
Luca Lee <30341309+lucalee-1@users.noreply.github.com>
2022-06-21 12:08:07 +0900

    Set auth token to expire after 24 hours

--- controllers/login.js
+++ controllers/login.js
@@ -6,26 +6,27 @@ const User = require('../models/user');
 loginRouter.post('/', async (req, res, next) => {
   const { username, password } = req.body;
 
-  try{
-  const user = await User.findOne({ username });
-  const passwordCorrect = user === null ? false : await bcrypt.compare(password, user.passwordHash);
-  if (!(user && passwordCorrect)) {
-    return res.status(401).json({
-      error: 'invalid username or password',
-    });
+  try {
+    const user = await User.findOne({ username });
+    const passwordCorrect =
+      user === null ? false : await bcrypt.compare(password, user.passwordHash);
+    if (!(user && passwordCorrect)) {
+      return res.status(401).json({
+        error: 'invalid username or password',
+      });
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user._id,
+    };
+
+    const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: 60 * 60 * 24});
+
+    res.status(200).send({ token, username: user.username, name: user.name });
+  } catch (error) {
+    next(error);
   }
-
-  const userForToken = {
-    username: user.username,
-    id: user._id,
-  };
-
-  const token = jwt.sign(userForToken, process.env.SECRET);
-
-  res.status(200).send({ token, username: user.username, name: user.name });
-} catch (error){
-  next(error)
-}
 });
 
 module.exports = loginRouter;
